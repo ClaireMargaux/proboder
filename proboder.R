30a4b51fced486f2c6fa75335d5be349b186d173
Claire Margaux Descombes <57965342+ClaireMargaux@users.noreply.github.com>
2024-03-05 14:09:26 +0100

    Functions for prediction and update steps

--- proboder.R
+++ proboder.R
@@ -66,16 +66,16 @@ observations <- left_join(susceptibles_per_day, cases_per_day, by = "date") %>%
   left_join(death_per_day, by = "date")
 
 #####################################
-############# ALGORITHM #############
+########## INITIALIZATION ###########
 #####################################
 
-# Initialization.
 X <- matrix(data= c(as.numeric(observations[1,2]),rep(0,11)),nrow = 12, ncol = 1)
 U <- 0
 P <- population
 
 library(Matrix)
 library(numDeriv)
+library(matrixcalc)
 
 #' @param recovery_rate
 #' @param fatality_rate
@@ -94,6 +94,9 @@ F_X <- sparseMatrix(i = 1:8, j = 5:12, x = 1, dims = c(12,12))
 # Dispersion matrices.
 L_U <- matrix(c(0,1), nrow = 2, ncol = 1)
 L_X <- sparseMatrix(i = 9:12, j = 1:4, x = 1, dims = c(12,4))
+
+# Observation matrix.
+H <- sparseMatrix(i = c(1,2,3), j = c(1,2,4), x = 1, dims = c(3,4))
 
 # Observation noise.
 R <- diag(1, nrow = 3, ncol = 3)
@@ -158,9 +161,107 @@ jacobian_matrix <- function(X,U){
 }
   
 # Prediction step.
+prediction <- function(m_X,P_X,m_U,P_U,F_X,F_U,L_X,L_U){
+  # Arguments:
+  #   arg1: m_X, vector(nrow=13), (previous) mean of X
+  #   arg2: P_X, matrix(nrow=12, ncol=12), (previous) covariance of X
+  #   arg3: m_U, numeric, (previous) mean of U
+  #   arg4: P_U, numeric, (previous) (co)variance of U
+  #   arg5: F_X, matrix(nrow=12, ncol=12), drif matrix of X
+  #   arg6: F_U, matrix(nrow=2, ncol=2), drif matrix of U
+  #   arg7: L_X, matrix(nrow=12, ncol=4), dispersion matrix of X
+  #   arg8: L_U, matrix(nrow=2, ncol=1), dispersion matrix of U
+  #
+  # Returns:
+  #   output: predicted means and covariances of X and U
+  
+  exp_F_X <- expm(F_X)
+  exp_F_U <- expm(F_U)
+  
+  m_X_out <- exp_F_X %*% m_X
+  m_U_out <- exp_F_U %*% m_U
+  
+  top_row_X <- cbind(F_X,L_X %*% t(L_X))
+  bottom_row_X <- cbind(matrix(0,nrow=12,ncol=12),-t(F_X))
+  
+  top_row_U <- cbind(F_U,L_U %*% t(L_U))
+  bottom_row_U <- cbind(matrix(0,nrow=2,ncol=2),-t(F_U))
 
+  Gamma_X <- rbind(top_row_X,bottom_row_X)
+  Gamma_U <- rbind(top_row_U,bottom_row_U)
+  
+  exp_Gamma_X <- expm(Gamma_X)
+  exp_Gamma_U <- expm(Gamma_U)
+  
+  M_1_X <- exp_Gamma_X[1:12,1:12]
+  M_2_X <- exp_Gamma_X[1:12,13:24]
+  
+  M_1_U <- exp_Gamma_U[1:2,1:2]
+  M_2_U <- exp_Gamma_U[1:2,3:4]
+
+  B_X <- M_2_X %*% t(M_1_X)
+  B_U <- M_2_U %*% t(M_1_U)
+  
+  P_X_out <- exp_F_X %*% P_X %*% t(exp_F_X) + B_X
+  P_U_out <- exp_F_U %*% P_U %*% t(exp_F_U) + B_U
+  
+  out <- list(m_X_out,P_X_out,m_U_out,P_U_out)
+  
+  return(out)
+}
 
 # Update step on tau_OBS.
-
+update_of_observations <- function(m,P,y,H,R){
+  # Arguments:
+  #   arg1: m, vector(nrow=13), predicted mean of U and X
+  #   arg2: P, matrix(nrow=13, ncol=13), predicted covariance of U and X
+  #   arg3: y, vector(nrow=y), observations
+  #   arg4: H, matrix(nrow=3, ncol=4), observation matrix
+  #   arg5: R, matrix(nrow=4, ncol=4), observation noise
+  #
+  # Returns:
+  #   output: vector(ncol=13) and matrix(nrow=13, ncol=13), updated
+  #           mean and covariances of U and X
+  
+  v <- y - H %*% m # residual
+  S <- H %*% P %*% t(P) + R # innovation covariance
+  S_inv <- svd.inverse(S)
+  K <- P %*% t(H) %*% S_inv # Kalman gain
+  m_out <- m + K %*% v # updated mean
+  P_out <- P - K %*% S %*% t(K) # updated covariance
+  
+  out <- list(m_out,P_out)
+  
+  return(out)
+}
 
 # Update step on tau_ODE.
+J <- jacobian_matrix(X,U)
+h <- h(X,U)
+update_of_states <- function(m,P,h,J){
+  # Arguments:
+  #   arg1: m, vector(nrow=13), predicted mean of U and X
+  #   arg2: P, matrix(nrow=13, ncol=13), predicted covariance of U and X
+  #   arg3, h, vector(nrow=4), measurement model
+  #   arg4: J, matrix(nrow=4, ncol=13), Jacobian of the measurement model
+  #
+  # Returns:
+  #   output: vector(ncol=13) and matrix(nrow=13, ncol=13), updated
+  #           mean and covariances of U and X
+  
+  v <- -h # residual
+  S <- J %*% P %*% t(J) # innovation covariance
+  S_inv <- svd.inverse(S)
+  K <- P %*% t(J) %*% S_inv # Kalman gain
+  m_out <- m + K %*% v # updated mean
+  P_out <- P - K %*% S %*% t(K) # updated covariance
+  
+  out <- list(m_out,P_out)
+  
+  return(out)
+}
+
+#####################################
+############# ALGORITHM #############
+#####################################
+
